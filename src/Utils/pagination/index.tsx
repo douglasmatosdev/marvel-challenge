7d951e5f8a79b11a9b33112dcbe0bb21b4f1cf4c
douglasmatosdev <douglasmatosdev@gmail.com>
2020-07-17 10:30:33 -0300

    Add and define pagination

--- src/Utils/pagination/index.tsx
+++ src/Utils/pagination/index.tsx
@@ -1,26 +1,26 @@
-import React, { FC, useState, useEffect } from 'react'
+import React, { FC, useState, useEffect, Fragment } from 'react'
 import { HeroContainer, HeroBox, HeroCard, ButtonsBox, Button } from './styled';
 
 import ApiServices from '../../Services/api'
 import { verifyApiKey } from '../../Utils/secure'
-import { count } from 'console'
 
 const Paginate: FC = () => {
-    const offset = 10
-    const limit = 10
-    const lastIndex = 1000
+    const offset = 5
+    const limit = 5
+    const [lastIndex, setLastIndex] = useState(0)
     const [heros, setHeros]: any[] = useState([])
     const [maxIndex, setMaxIndex] = useState(0)
     const [minIndex, setMinIndex] = useState(0)
     const [pageCount, setPageCount] = useState(0)
     const [paginateInfo, setPaginationInfo] = useState({})
+    const [loading, setLoading] = useState(true)
 
     const setPagination = (counter = 0) => {
         if (verifyApiKey()) {
             ApiServices.allHeros(counter, limit, null)
                 .then(res => {
                     const data = res.data.data
-
+                    setLastIndex(data.total)
                     setHeros(data.results)
                     setPaginationInfo({
                         ...paginateInfo,
@@ -29,6 +29,7 @@ const Paginate: FC = () => {
                         total: data.total,
                         count: data.count
                     })
+                    setLoading(false)
                 })
         }
     }
@@ -40,48 +41,55 @@ const Paginate: FC = () => {
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [minIndex, maxIndex])
-
+    console.log(paginateInfo)
     return (
         <HeroContainer>
-            <HeroBox>
-                {heros?.map((char: any, i: number) => (
-                    <HeroCard className="herocard" key={char.id}>
-                        <img src={`${char.thumbnail.path}.${char.thumbnail.extension}`} alt="" />
-                        <strong>{char.name}</strong>
-                        <div className="canto-chanfrado"></div>
-                    </HeroCard>
-                ))}
-            </HeroBox>
-            <ButtonsBox className="actions">
-                <Button
-                    className="paginate-button paginate-back"
-                    onClick={() => {
-                        if (minIndex > 0) {
-                            setMaxIndex(minIndex)
-                            setMinIndex(minIndex - offset)
-                            setPageCount(pageCount - 1)
-                        }
-                    }}
-                >
-                    {'< Back'}
-                </Button>
-                <Button style={{ background: 'green' }} className="page-counter">
-                    {pageCount + 1} / {lastIndex / offset}
-                </Button>
-                <Button
-                    className="paginate-button paginate-next"
-                    onClick={() => {
-                        if (maxIndex < lastIndex) {
-                            setMinIndex(maxIndex)
-                            setMaxIndex(minIndex + offset)
-                            setPageCount(pageCount + 1)
-                        }
-                    }}
-                >
-                    {'Next >'}
-                </Button>
+            {!loading ? (
+                <Fragment>
+                    <HeroBox>
+                        {heros?.map((char: any, i: number) => (
+                            <HeroCard className="herocard" key={char.id}>
+                                <img src={`${char.thumbnail.path}.${char.thumbnail.extension}`} alt={char.name} />
+                                <strong>{char.name}</strong>
+                                <div className="canto-chanfrado"></div>
+                            </HeroCard>
+                        ))}
+                    </HeroBox>
+                    <ButtonsBox className="actions">
+                        <Button
+                            className="paginate-button paginate-back"
+                            onClick={() => {
+                                if (minIndex > 0) {
+                                    setMaxIndex(minIndex)
+                                    setMinIndex(minIndex - offset)
+                                    setPageCount(pageCount - 1)
+                                }
+                            }}
+                        >
+                            {'< Back'}
+                        </Button>
+                        <Button className="page-counter">
+                            {pageCount + 1} / {lastIndex / offset}
+                        </Button>
+                        <Button
+                            className="paginate-button paginate-next"
+                            onClick={() => {
+                                if (maxIndex < lastIndex) {
+                                    setMinIndex(maxIndex)
+                                    setMaxIndex(minIndex + offset)
+                                    setPageCount(pageCount + 1)
+                                }
+                            }}
+                        >
+                            {'Next >'}
+                        </Button>
 
-            </ButtonsBox>
+                    </ButtonsBox>
+                </Fragment>
+            ) : (
+                    <h1>Carregando...</h1>
+                )}
+
         </HeroContainer>
     )
 }
